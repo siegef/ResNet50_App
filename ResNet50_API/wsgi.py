b2d0d8b96182cb0c16596690de30604e97e7ca1e
Christian Flores <christian18f@gmail.com>
2020-10-29 09:26:50 +0800

    Initial setup

---
+++ ResNet50_API/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ResNet50_API project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ResNet50_API.settings')
+
+application = get_wsgi_application()
---
+++ resnet50/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Resnet50Config(AppConfig):
+    name = 'resnet50'
