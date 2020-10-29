d43e3eb5e3bd04a4a3899b916a92c2901a8b9335
Christian Flores <christian18f@gmail.com>
2020-10-29 10:00:19 +0800

    Added basic form

--- resnet50/views.py
+++ resnet50/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from django import forms
+
+class UploadFileForm(forms.Form):
+    file = forms.ImageField(widget=forms.ClearableFileInput(attrs={'class':'custom-file-input'}));
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    form = UploadFileForm()
+    if request.method == "POST":
+        form = UploadFileForm(request.POST)
+        return render(request, "resnet50/index.html")
+    else:
+        return render(request, "resnet50/index.html", {
+            'form' : form,
+        })
